871de6614710ac70b8337b13930738a604a35e5a
Nick Dunlap <nwdunlap17@gmail.com>
2019-08-21 19:52:21 -0700

    cipher

---
+++ cipher.rb
@@ -0,0 +1,32 @@
+def cipher(line)
+    numb = "0123456789"#.split('').delete('')
+    arr = []
+    line.length.times do |i|
+        if numb.include?(line[i])
+            arr << line[i]
+        end
+    end
+
+    letter = []
+
+    first = false
+    arr.each do |num|
+        if !first && num.to_i < 3
+            first = num.to_i
+        elsif first && (first*10 + num.to_i) < 26
+            letter <<  ((first*10) + (num.to_i))
+            first = false
+        end
+    end
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+    letter.each do |letter|
+        print alpha[letter]
+    end
+    puts ''
+end
+
+# lines = ARGF.read.split("\n")
+# lines.each do |line|
+#     puts cipher(line)
+# end
+cipher("02lyne176xbs1436d22gk25dm")
